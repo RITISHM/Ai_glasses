e4983c5718ba4c5fe31522b83a8d5b6f20e64deb
RITISHM <ritishmahajan15f@gmail.com>
2025-10-13 02:30:07 +0530

    basic changes

--- server.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-tts = gTTS(
-    text="high mera name google hai. me ek assistant hu jo aapki madad karega ",
-    lang='en',
-    tld='co.in',     # This gives you Indian English accent
-    slow=False
-)
-tts.save("indian_accent.mp3")
