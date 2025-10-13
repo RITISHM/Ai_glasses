f13fc98e6a07faf6d89460c177c806f36dc4f5dd
RITISHM <ritishmahajan15f@gmail.com>
2025-10-13 06:11:00 +0530

    added stt and tts

---
+++ stt.py
@@ -0,0 +1,18 @@
+from faster_whisper import WhisperModel
+import torch
+
+
+# Load the model python s 
+# arge-v2 is high quality; choose smaller models for speed)
+model_size = "small"
+model = WhisperModel(model_size, device="cuda")  # force GPU usage
+
+# Path to your audio file (WAV or MP3)
+audio_path = "indian_accent.mp3"
+for i in range(5):
+# Transcribe
+    segments, info = model.transcribe(audio_path, beam_size=10,language="en")
+
+
+    for segment in segments:
+        
